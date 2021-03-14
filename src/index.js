94c13ff3b823d4266ba8a647612052087f26355b
Irina-coud <irinatobolich313@gmail.com>
2021-03-14 12:49:55 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  }
+  const resultArray = matrix.reduce((acc, array, index) => {
+    const isEven = index % 2 === 0;
+
+    if (isEven) {
+      acc = acc.concat(array);
+    } else {
+      acc = acc.concat(array.reverse());
+    }
+    return acc;
+  }, []);
+  return resultArray;
 }
